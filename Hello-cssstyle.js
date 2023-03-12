ac29e46f8b41003ef3c009d7be8b5ddaad8b78c5
ffccsm <mpisohel14@gmail.com>
2023-03-13 02:28:09 +0600

    css class and autio add process

---
+++ Hello-cssstyle.js
@@ -0,0 +1,17 @@
+// Get the paragraph element and the button element
+
+
+
+// Add an event listener to the button
+
+
+
+
+    function myStyle() {
+
+    var myParagraph = document.querySelector("#myId");
+
+    myParagraph.classList.add ("para-style");
+
+    
+}
---
+++ audioplay.js
@@ -0,0 +1,40 @@
+
+
+
+function myStyle() {
+
+    document.querySelector("button");
+
+
+
+var audio = new Audio("Sounds/1.mp3");
+
+audio.oncanplaythrough = function(){
+audio.play();
+}
+
+audio.loop = true;
+
+audio.onended = function(){
+audio.play();
+}
+}
+
+function myStyle2() {
+
+    document.querySelector("button");
+
+
+
+var audio = new Audio("Sounds/2.mp3");
+
+audio.oncanplaythrough = function(){
+audio.play();
+}
+
+audio.loop = true;
+
+audio.onended = function(){
+audio.play();
+}
+}
