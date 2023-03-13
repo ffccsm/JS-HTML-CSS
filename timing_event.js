8f09b78aa21dcc2991d250b9dbdf69b9f2c28107
ffccsm <mpisohel14@gmail.com>
2023-03-14 00:30:29 +0600

    timing setInterval added

---
+++ timing_event.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+setTimeout(()=>{
+
+    var myVar = document.querySelector("#paraId");
+
+    myVar.classList.add("para-class");
+
+}, 2000)
+
+
+
+
+
+setInterval(()=> {
+
+
+    var myVar2 = document.querySelector("#paraId");
+
+    myVar2.classList.remove("para-class");
+
+}, 10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction(){
+
+    
+
+
+// }
+
+
+
+
+// function removeCss(){
+
+//     var myVar2 = document.querySelector("#paraId");
+
+//     myVar2.classList.remove("para-class");
+
+
+// }
