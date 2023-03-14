cf3dd173582539e178f9a2fba9d7bf9f8b9651c8
ffccsm <mpisohel14@gmail.com>
2023-03-15 01:04:18 +0600

    taking input from user and print it

---
+++ input.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+document.querySelector("#buttonDemo").addEventListener('click', function () {
+
+   var num = document.querySelector("#inputDemo").value;
+
+   document.querySelector("#demo").innerHTML = num;
+
+
+
+
+
+
+
+
+
+})
